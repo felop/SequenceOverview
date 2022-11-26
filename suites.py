aa766683c98f29331d12ba65a63f9ebef3cf1946
Felox <contact@felixlop.com>
2022-11-26 10:19:19 +0100

    added l/d themes

--- suites.py
+++ suites.py
@@ -2,40 +2,37 @@ import pygame, math
 from pygame import gfxdraw
 
 def init():
-    screen.fill(bg_color)
+    screen.fill(colors[th]["bg"])
 
-    grid_color = (50,50,50)
-    text_color = (150,150,150)
+    pygame.draw.line(screen, colors[th]["bissec"], (input_size[0],0), (0,input_size[1]))
+    for dist in range(thickness["bissec"]-1):
+        pygame.draw.line(screen, colors[th]["bissec"], (input_size[0]-1*dist,0), (0,input_size[1]-1*dist))
+        pygame.draw.line(screen, colors[th]["bissec"], (input_size[0],dist), (0,input_size[1]+1*dist))
+
     for LineX in range(input_scale[0]+1):
-        pygame.draw.line(screen, grid_color, (LineX*(input_size[0]/input_scale[0]),0), (LineX*(input_size[0]/input_scale[0]),input_size[1]))
+        pygame.draw.line(screen, colors[th]["grid"], (LineX*(input_size[0]/input_scale[0]),0), (LineX*(input_size[0]/input_scale[0]),input_size[1]))
         if LineX <= input_scale[0]:
-            val_x = text_font.render(str(int(LineX-input_scale[0]/2)), True, text_color)
+            val_x = text_font.render(str(int(LineX-input_scale[0]/2)), True, colors[th]["text"])
             screen.blit(val_x, (LineX*(input_size[0]/input_scale[0]) -12,input_size[1]/2 +5))
 
     for LineY in range(input_scale[1]+1):
-        pygame.draw.line(screen, grid_color, (0,LineY*(input_size[1]/input_scale[1])), (input_size[0],LineY*(input_size[1]/input_scale[1])))
+        pygame.draw.line(screen, colors[th]["grid"], (0,LineY*(input_size[1]/input_scale[1])), (input_size[0],LineY*(input_size[1]/input_scale[1])))
         if LineY <= input_scale[1]:
-            val_y = text_font.render(str(-int(LineY-input_scale[1]/2)), True, text_color)
+            val_y = text_font.render(str(-int(LineY-input_scale[1]/2)), True, colors[th]["text"])
             screen.blit(val_y, (input_size[0]/2 -12, LineY*(input_size[1]/input_scale[1]) +5))
 
-    axes_color = (100,255,0)
-    pygame.draw.line(screen, axes_color, (input_size[0]/2,0), (input_size[0]/2,input_size[1]))
-    pygame.draw.line(screen, axes_color, (0,input_size[1]/2), (input_size[0],input_size[1]/2))
-
-    bissec_color = (40,130,0)
-    #pygame.draw.line(screen, bissec_color, (0,0), (input_size[0],input_size[1]))
-    pygame.draw.line(screen, bissec_color, (input_size[0],0), (0,input_size[1]))
+    pygame.draw.line(screen, colors[th]["axes"], (input_size[0]/2,0), (input_size[0]/2,input_size[1]))
+    pygame.draw.line(screen, colors[th]["axes"], (0,input_size[1]/2), (input_size[0],input_size[1]/2))
 
     #### OUTPUT ####
     graph = pygame.Rect(input_size[0]+border_size[0], border_size[1], output_size[0], output_size[1])
-    pygame.draw.rect(screen, (50,50,50), graph)
+    pygame.draw.rect(screen, colors[th]["rect"], graph)
 
 def update():
     mouse_pos = pygame.mouse.get_pos()
     mouse_pos = min(input_size[1],mouse_pos[0]),min(input_size[1],mouse_pos[1])
-    mouse_pos = (125*1.5,125*3)
+#    mouse_pos = (125*1.5,250)
 
-    func_color = (100,255,255)
     global func
     def func(x):
         f = lambda x: 9/(6-x)#-1.1*x-2
@@ -53,33 +50,32 @@ def update():
     for val_x in range(input_size[0]):
         defined, val_y = func(val_x/input_size[0]*input_scale[0]-input_scale[0]/2)
         if defined:
-            pygame.gfxdraw.pixel(screen, val_x, int((-val_y+input_scale[0]/2)*input_size[0]/input_scale[0]), func_color)
+            pygame.gfxdraw.pixel(screen, val_x, int((-val_y+input_scale[0]/2)*input_size[0]/input_scale[0]), colors[th]["func"])
+            for dist in range(thickness["func"]-1):
+                pygame.gfxdraw.pixel(screen, val_x, int((-val_y+input_scale[0]/2)*input_size[0]/input_scale[0])-1*dist, colors[th]["func"])
+                pygame.gfxdraw.pixel(screen, val_x, int((-val_y+input_scale[0]/2)*input_size[0]/input_scale[0])+1*dist, colors[th]["func"])
 
-
-    u0_line_color = (255,150,50)
-    pygame.draw.line(screen, u0_line_color, mouse_pos, (mouse_pos[0],input_size[1]/2))
-    u0_text = text_font.render("U0", True, u0_line_color)
+    pygame.draw.line(screen, colors[th]["u0_line"], mouse_pos, (mouse_pos[0],input_size[1]/2))
+    u0_text = text_font.render("U0", True, colors[th]["u0_line"])
     screen.blit(u0_text, (mouse_pos[0]-17,input_size[1]/2-13))
 
-    u1_line_color = (255,100,100)
     prev_mirrored = (mouse_pos[0], input_size[1]/2)
     image, mirrored, defined = step(prev_mirrored[0])
     boundaries = lambda x: x[0] >= 0 and x[1] >= 0 and x[0] <= input_size[0] and x[1] <= input_size[1]
     i = 0
     if defined:
         while math.sqrt((image[0]-mirrored[0])**2+ (image[1]-mirrored[1])**2) > 3 and boundaries(image) and boundaries(mirrored):
-            pygame.draw.line(screen, u1_line_color, (int(prev_mirrored[0]),int(prev_mirrored[1])), (int(image[0]),int(image[1])))
-            pygame.draw.line(screen, u1_line_color, (int(image[0]),int(image[1])), (int(mirrored[0]),int(mirrored[1])))
+            pygame.draw.line(screen, colors[th]["u1_line"], (int(prev_mirrored[0]),int(prev_mirrored[1])), (int(image[0]),int(image[1])))
+            pygame.draw.line(screen, colors[th]["u1_line"], (int(image[0]),int(image[1])), (int(mirrored[0]),int(mirrored[1])))
             prev_mirrored = mirrored
             if i == 0:
-                pygame.draw.line(screen, (255,255,255), (image[0]-3,image[1]), (image[0]+3,image[1]))
-                pygame.draw.line(screen, (255,255,255), (image[0],image[1]-3), (image[0],image[1]+3))
+                pygame.draw.line(screen, colors[th]["pointer"], (image[0]-3,image[1]), (image[0]+3,image[1]))
+                pygame.draw.line(screen, colors[th]["pointer"], (image[0],image[1]-3), (image[0],image[1]+3))
             i += 1
             image, mirrored,_ = step(prev_mirrored[0])
 
-    pointer_color = (255,255,255)
-    pygame.draw.line(screen, pointer_color, (mouse_pos[0]-5,mouse_pos[1]), (mouse_pos[0]+5,mouse_pos[1]))
-    pygame.draw.line(screen, pointer_color, (mouse_pos[0],mouse_pos[1]-5), (mouse_pos[0],mouse_pos[1]+5))
+    pygame.draw.line(screen, colors[th]["pointer"], (mouse_pos[0]-5,mouse_pos[1]), (mouse_pos[0]+5,mouse_pos[1]))
+    pygame.draw.line(screen, colors[th]["pointer"], (mouse_pos[0],mouse_pos[1]-5), (mouse_pos[0],mouse_pos[1]+5))
 
     #### OUTPUT ####
 
@@ -94,11 +90,11 @@ def update():
         if output_scale[0] != output_scale[1]:
             for i in range(len(first_images)):
                 x, y = input_size[0]+border_size[0]+int(output_size[0]/15*i), border_size[1]+output_size[1]-(first_images[i]-output_scale[1])/(output_scale[0]-output_scale[1])*output_size[1]
-                pygame.draw.line(screen, u1_line_color, (x-5, y), (x+5, y))
-                pygame.draw.line(screen, u1_line_color, (x, y-5), (x, y+5))
-                u_text = text_font.render("U"+str(i), True, u0_line_color)
+                pygame.draw.line(screen, colors[th]["u1_line"], (x-5, y), (x+5, y))
+                pygame.draw.line(screen, colors[th]["u1_line"], (x, y-5), (x, y+5))
+                u_text = text_font.render("U"+str(i), True, colors[th]["u0_line"])
                 screen.blit(u_text, (x-17,y+2))
-                u_text = text_font.render(str(round(first_images[i],3)), True, (200,200,200))
+                u_text = text_font.render(str(round(first_images[i],3)), True, colors[th]["outputText"])
                 screen.blit(u_text, (x-10,y+13))
 
 def step(pos):
@@ -116,12 +112,45 @@ def step(pos):
             return None, None, False
 
 func = lambda x: x
-bg_color = (0,0,0)
 size = (1300,500)
 input_size = (500,500)
 input_scale = (8,8)
 border_size = (20,30)
 output_size = (size[0]-input_size[0]-2*border_size[0], size[1]-2*border_size[1])
+thickness = {
+    "func" : 3,
+    "bissec" : 3,
+}
+th = ["light","dark"][1]
+
+colors = {
+    "light" : {
+        "bg" : (255,255,255),
+        "axes" : (0,150,0),
+        "bissec" : (20,190,20),
+        "grid" : (150,150,150),
+        "text" : (0,0,0),
+        "rect" : (230,230,230),
+        "func" : (0,200,200),
+        "u0_line" : (255,150,50),
+        "u1_line" : (200,60,60),
+        "pointer" : (0,0,0),
+        "outputText" : (50,50,50)
+    },
+    "dark" : {
+        "bg" : (0,0,0),
+        "axes" : (100,255,0),
+        "bissec" : (40,130,0),
+        "grid" : (50,50,50),
+        "text" : (150,150,150),
+        "rect" : (50,50,50),
+        "func" : (100,255,255),
+        "u0_line" : (255,150,50),
+        "u1_line" : (255,100,100),
+        "pointer" : (255,255,255),
+        "outputText" : (200,200,200)
+    }
+}
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('(Un ; Un+1)')
