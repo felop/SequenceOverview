ba39be85bad155ca7ea0f46b7e637c91ebc238d9
felop <contact@felixlop.com>
2023-10-20 14:51:05 +0200

    added "input_origin" (offsets origin and axis)

--- suites.py
+++ suites.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, os
 from pygame import gfxdraw
 from pytexit import py2tex
 import webbrowser
@@ -6,30 +6,53 @@ import webbrowser
 def init():
     #### INPUT ####
     screen.fill(colors[th]["bg"])
+    drawBissec(thickness["bissec"])
+    xAxisOff = (input_origin[1] if abs(input_origin[1]) <= input_scale[1]/2 else None)
+    yAxisOff = (input_origin[0] if abs(input_origin[0]) <= input_scale[0]/2 else None)
+    #print(xAxisOff, yAxisOff)
+    drawGrid(xAxisOff, yAxisOff)
+    drawAxis(xAxisOff, yAxisOff)
 
-    pygame.draw.line(screen, colors[th]["bissec"], (input_size[0],0), (0,input_size[1]))
-    for dist in range(1,thickness["bissec"]+1):
-        pygame.draw.line(screen, colors[th]["bissec"], (input_size[0]-1*dist,0), (0,input_size[1]-1*dist))
-        pygame.draw.line(screen, colors[th]["bissec"], (input_size[0],dist), (0,input_size[1]+1*dist))
+    #### OUTPUT ####
+    graph = pygame.Rect(input_size[0] + border_size[0], border_size[1], output_size[0], output_size[1])
+    pygame.draw.rect(screen, colors[th]["rect"], graph)
 
+def drawBissec(width):
+    width = math.floor(width/2)
+    for val_x in range(input_size[0]):
+        x = val_x/input_size[0]*input_scale[0]-input_scale[0]/2+input_origin[0]
+        val_y = x+input_origin[0]-input_origin[1]
+        val_y = (val_y-input_origin[0]+input_scale[0]/2)/input_scale[0]*input_size[0]
+        pygame.draw.line(screen, colors[th]["bissec"], (val_x, input_size[1]-val_y-1*width),(val_x, input_size[1]-val_y+1*(width+1)))
+
+def drawGrid(xOff,yOff):
     for LineX in range(input_scale[0]+1):
         pygame.draw.line(screen, colors[th]["grid"], (LineX*(input_size[0]/input_scale[0]),0), (LineX*(input_size[0]/input_scale[0]),input_size[1]))
-        if LineX <= input_scale[0]:
-            val_x = text_font.render(str(int(LineX-input_scale[0]/2)), True, colors[th]["text"])
-            screen.blit(val_x, (LineX*(input_size[0]/input_scale[0]) -12,input_size[1]/2 +5))
+        val_x = text_font.render(str(int(LineX - input_scale[0] / 2 + input_origin[0])), True, colors[th]["text"])
+        if xOff is not None:
+            screen.blit(val_x, (LineX*(input_size[0]/input_scale[0]) -12,input_size[1]/2+xOff*input_size[1]/input_scale[1] +5))
+        else:
+            if input_origin[1] > input_scale[1]/2:
+                screen.blit(val_x, (LineX*(input_size[0]/input_scale[0]) -12, input_size[1] -15))
+            else:
+                screen.blit(val_x, (LineX*(input_size[0]/input_scale[0]) -12, 5))
 
     for LineY in range(input_scale[1]+1):
         pygame.draw.line(screen, colors[th]["grid"], (0,LineY*(input_size[1]/input_scale[1])), (input_size[0],LineY*(input_size[1]/input_scale[1])))
-        if LineY <= input_scale[1]:
-            val_y = text_font.render(str(-int(LineY-input_scale[1]/2)), True, colors[th]["text"])
-            screen.blit(val_y, (input_size[0]/2 -12, LineY*(input_size[1]/input_scale[1]) +5))
+        val_y = text_font.render(str(-int(LineY - input_scale[1] / 2 - input_origin[1])), True, colors[th]["text"])
+        if yOff is not None:
+            screen.blit(val_y, (input_size[0]/2-yOff*input_size[0]/input_scale[0] -12, LineY*(input_size[1]/input_scale[1]) +5))
+        else:
+            if input_origin[0] > input_scale[0]/2: #CH A GAUCHE
+                screen.blit(val_y, (5, LineY*(input_size[1]/input_scale[1]) +5))
+            else:
+                screen.blit(val_y, (input_size[0]-12, LineY*(input_size[1]/input_scale[1]) +5))
 
-    pygame.draw.line(screen, colors[th]["axes"], (input_size[0]/2,0), (input_size[0]/2,input_size[1]))
-    pygame.draw.line(screen, colors[th]["axes"], (0,input_size[1]/2), (input_size[0],input_size[1]/2))
-
-    #### OUTPUT ####
-    graph = pygame.Rect(input_size[0]+border_size[0], border_size[1], output_size[0], output_size[1])
-    pygame.draw.rect(screen, colors[th]["rect"], graph)
+def drawAxis(xOff,yOff):
+    if xOff is not None:
+        pygame.draw.line(screen, colors[th]["axes"], (0,input_size[1]/2+xOff*input_size[1]/input_scale[1]), (input_size[0],input_size[1]/2+xOff*input_size[1]/input_scale[1]))
+    if yOff is not None:
+        pygame.draw.line(screen, colors[th]["axes"], (input_size[0]/2-yOff*input_size[0]/input_scale[0],0), (input_size[0]/2-yOff*input_size[0]/input_scale[0],input_size[1]))
 
 def update():
     #### INPUT ####
@@ -90,7 +113,7 @@ def update():
                 pygame.draw.line(screen, colors[th]["u1_line"], (int(image[0]),int(image[1])+1*dist), (int(mirrored[0]),int(mirrored[1])+1*dist))
 
             prev_mirrored = mirrored
-            if i == 0:
+            if i%input_pointerFreq == 0:
                 pygame.draw.line(screen, colors[th]["pointer"], (image[0]-3,image[1]), (image[0]+3,image[1]))
                 pygame.draw.line(screen, colors[th]["pointer"], (image[0],image[1]-3), (image[0],image[1]+3))
             i += 1
@@ -100,22 +123,22 @@ def update():
     pygame.draw.line(screen, colors[th]["pointer"], (mouse_pos[0],mouse_pos[1]-5), (mouse_pos[0],mouse_pos[1]+5))
 
     #### OUTPUT ####
-    first_images = [mouse_pos[0]/input_size[0]*input_scale[0]-input_scale[0]/2]
+    first_y = [mouse_pos[0]/input_size[0]*input_scale[0]-input_scale[0]/2+input_origin[0]]
     for _ in range(1,15+1):
-        defined, funcResult = func(first_images[-1])
+        defined, funcResult = func(first_y[-1]-input_origin[0])
         if not defined:
             break
-        first_images.append(funcResult)
+        first_y.append(funcResult+input_origin[1])
     if defined:
-        output_scale = [max(first_images),min(first_images)]
+        output_scale = [max(first_y),min(first_y)]
         if output_scale[0] != output_scale[1]:
-            for i in range(len(first_images)):
-                x, y = input_size[0]+border_size[0]+int(output_size[0]/15*i), border_size[1]+output_size[1]-(first_images[i]-output_scale[1])/(output_scale[0]-output_scale[1])*output_size[1]
+            for i in range(len(first_y)):
+                x, y = input_size[0]+border_size[0]+int(output_size[0]/15*i), border_size[1]+output_size[1]-(first_y[i]-output_scale[1])/(output_scale[0]-output_scale[1])*output_size[1]
                 pygame.draw.line(screen, colors[th]["u1_line"], (x-5, y), (x+5, y))
                 pygame.draw.line(screen, colors[th]["u1_line"], (x, y-5), (x, y+5))
                 u_text = text_font.render("U"+str(i), True, colors[th]["u0_line"])
                 screen.blit(u_text, (x-17,y+2))
-                u_text = text_font.render(str(round(first_images[i],3)), True, colors[th]["outputText"])
+                u_text = text_font.render(str(round(first_y[i],3)), True, colors[th]["outputText"])
                 screen.blit(u_text, (x-10,y+13))
 
 def step(pos):
@@ -127,21 +150,25 @@ def step(pos):
         defined, funcResult = func(pos/input_size[0]*input_scale[0]-input_scale[0]/2)
         if defined:
             image = (pos,(-funcResult+input_scale[0]/2)*input_size[0]/input_scale[0])
-            mirrored = (min(input_size[0]/2 + input_size[1]/2-image[1], input_size[0]),image[1])
+            mirrored = (min(input_size[0]/2+input_size[1]/2-image[1], input_size[0]) -input_origin[0]*(input_size[0]/input_scale[0]), image[1])
             return image, mirrored, True
         else:
             return None, None, False
 
 func = lambda x: x
 stringF = "1/8*(x+2)*(11-2*x)" #"x/math.sqrt(x+4) +2"
-f = lambda x: eval(stringF)
+fEval = lambda x: eval(stringF)
+f = lambda x: fEval(x+input_origin[0])-input_origin[1]
 size = (1300,501)
 input_size = (500,500)
-input_scale = (8,8)
+input_scale = (8, 8)
+input_origin = (0, 0)
+input_pointerFreq = 1 # number of skipped crosses  (1 is min)
 border_size = (20,30)
 output_size = (size[0]-input_size[0]-2*border_size[0], size[1]-2*border_size[1])
-U0 = (-34/10,-31/10)
-fixed_U0 = [None, (lambda x,y : [input_size[0]/input_scale[0]*(x+input_scale[0]/2),input_size[1]/input_scale[1]*(-y+input_scale[1]/2)])(U0[0],U0[1])][1]
+U0 = (-34/10, -31/10)
+
+fixed_U0 = [None, (lambda x,y : [input_size[0]/input_scale[0]*(x+input_scale[0]/2-input_origin[0]),input_size[1]/input_scale[1]*(-y+input_scale[1]/2+input_origin[1])])(U0[0],U0[1])][0]
 thickness = {
     "func" : 1,
     "bissec" : 1,
@@ -179,9 +206,14 @@ colors = {
 }
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('(Un ; Un+1)')
+pygame.display.set_caption('Un+1 = f(Un)')
 pygame.font.init()
 text_font = pygame.font.SysFont(None, 20)
+
+if input_scale[0]%2 == 1 or input_scale[1]%2 == 1:
+    input_scale_oldRef = input_scale
+    input_scale = (input_scale[0] + input_scale[0] % 2, input_scale[1] + input_scale[1] % 2)
+    print(f'WARNING : input scale {input_scale_oldRef} -> {input_scale}')
 
 running = True
 while running:
@@ -203,4 +235,4 @@ while running:
             lines[17] = "      f(x) = "+ py2tex(stringF,print_formula=False,print_latex=False)[2:-2] +" \;\;\; u_{n+1} = f(u_n) \;\;\; u_0 = "+ str(round(U0[0],2)) +" \n"
         with open("index.html","w+") as htmlPage:
             htmlPage.write("".join(lines))
-        webbrowser.open_new_tab("index.html")
+        webbrowser.open_new_tab("file://"+os.path.realpath("index.html"))
